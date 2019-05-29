ba90f3180129113d2542862104698d6db93451dc
Zachary Hislop <zhislop@calpoly.edu>
2019-05-28 18:22:01 -0700

    add play_note subroutine, add example usage and sample of constants

--- jukebox.asm
+++ jukebox.asm
@@ -1,26 +1,52 @@
-; Constants for delay loop
+; DELAY constants
 .EQU INNER_LOOP = 0xFF
 .EQU MIDDLE_LOOP = 0xFF
 
+; PORT Definitions
+.EQU SPEAKER_PORT = 0x69
 
-MOV   	      R31, 0x19 ;0.0996s
+; NOTE Defintions
+.EQU LOWC = 0xNUM 	;NUM to be filled with correct value
+
+.EQU EIGHTH = 0xNUM 	;NUM to be filled with correct value
+
+; Example Usage
+		MOV 	R4, LOWC 	; Set note to LOWC
+		MOV   	R31, EIGHTH 	; Set note length to 1/8 note
+		CALL 	play_note 	; Call play_note subroutine
 
 
-;Delay subroutine
-; R31 = 0x0E => 0.0734s delay
-; R31 = 0x60 => 0.503 delay
-; Parameters R31: outer for loop count
-; Affected R1, R2, R3
 
-delay:	      MOV R1, R31  ;set outside for loop count
-outside_one:  SUB R1, 0x01
-              MOV R2, MIDDLE_LOOP	 ;set middle for loop count
-middle_one:   SUB R2, 0x01
-              MOV R3, INNER_LOOP 	 ;set inside for loop count
-inside_one:   SUB R3, 0x01
-              BRNE inside_one
-              OR R2, 0x00           ;load flags for middle for counter
-              BRNE middle_one
-              OR R1, 0x00           ;load flags for outsde for counter value
-              BRNE outside_one		
-	      RET
+
+; Play Note Subroutine
+; Descr: Outputs note to Speaker port, calls delay subroutine for proper time
+; Parameters: 
+; 	- R4: Note to be played
+; Affected:
+; 	- R1, R2, R3 (in delay subroutine)
+play_note: 	OUT R4, SPEAKER_PORT
+		CALL 	delay
+		RET
+
+
+; Delay subroutine
+; Descr: Delays for time specified by R31
+; 	- R31 = 0x0E => 0.0734s delay
+; 	- R31 = 0x60 => 0.503 delay
+; Parameters:
+; 	- R31: outer for loop count
+; Affected:
+; 	- R1, R2, R3
+
+delay: 		MOV R1, R31  ;set outside for loop count
+outside_one: 	SUB R1, 0x01
+              	MOV R2, MIDDLE_LOOP	 ;set middle for loop count
+middle_one:   	SUB R2, 0x01
+              	MOV R3, INNER_LOOP 	 ;set inside for loop count
+inside_one:   	SUB R3, 0x01
+		BRNE inside_one
+              	OR R2, 0x00           ;load flags for middle for counter
+              	BRNE middle_one
+              	OR R1, 0x00           ;load flags for outsde for counter value
+              	BRNE outside_one		
+	      	RET
