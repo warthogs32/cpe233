a16527a123ccbdd3128c014ce0f73c3035e730a6
Zachary Hislop <zhislop@calpoly.edu>
2019-05-30 10:01:48 -0700

    fix note number and relative lengths. add 7 nation army subroutine

--- jukebox.asm
+++ jukebox.asm
@@ -6,15 +6,91 @@
 .EQU SPEAKER_PORT = 0x69
 
 ; NOTE Defintions
-.EQU LOWC = 0xNUM 	;NUM to be filled with correct value
+.EQU	SIXTEENTH = 0X01
+.EQU	EIGHTH = 0X02
+.EQU	QUARTER = 0X04
+.EQU 	HALF = 0X08
+.EQU 	WHOLE = 0X10
 
-.EQU EIGHTH = 0xNUM 	;NUM to be filled with correct value
+.EQU	REST 	= 0
+.EQU	LOC  	= 1
+.EQU	LOC# 	= 2
+.EQU	LOD  	= 3
+.EQU 	LOD# 	= 4
+.EQU	LOE  	= 5
+.EQU	LOF  	= 6
+.EQU	LOF# 	= 7
+.EQU	LOG  	= 8
+.EQU	LOG# 	= 9
+.EQU	LOA  	= 10
+.EQU 	LOA# 	= 11
+.EQU	LOB  	= 12
+.EQU	MIDC 	= 13
+.EQU	MIDC# 	= 14
+.EQU	MIDD  	= 15
+.EQU 	MIDD# 	= 16
+.EQU	MIDE  	= 17
+.EQU	MIDF  	= 18
+.EQU	MIDF# 	= 19
+.EQU	MIDG  	= 20
+.EQU	MIDG# 	= 21
+.EQU	MIDA  	= 22
+.EQU 	MIDA# 	= 23
+.EQU	MIDB  	= 24
+.EQU	HIC 	= 25
+.EQU	HIC#	= 26
+.EQU	HID 	= 27
+.EQU 	HID#	= 28
+.EQU	HIE 	= 29
+.EQU	HIF 	= 30
+.EQU	HIF#	= 31
+.EQU	HIG 	= 32
+.EQU	HIG#	= 33
+.EQU	HIA 	= 34
+.EQU 	HIA#	= 35
+.EQU	HIB 	= 36
 
-; Example Usage
-		MOV 	R4, LOWC 	; Set note to LOWC
-		MOV   	R31, EIGHTH 	; Set note length to 1/8 note
-		CALL 	play_note 	; Call play_note subroutine
 
+main: 		IN 		R6, LOWER_BUTTONS 	; Read song number from buttons
+		CALL 		R6 			; Branch to correct song number
+		BRN 		main
+
+;Seven nation army
+
+;The White Stripes  Seven Nation Army  126 BPM
+
+1:
+Seven_NA:	MOV		R4, MIDE	
+		MOV		R31, SIXTEENTH
+		CALL 		play_note
+		MOV		R31, SIXTEENTH
+		CALL 		play_note
+		MOV		R31, SIXTEENTH
+		CALL 		play_note
+		CALL 		play_note
+		MOV		R4, MIDE	
+		MOV		R31, EIGHTH
+		CALL 		play_note
+		MOV		R4, MIDG	
+		MOV		R31, EIGHTH
+		CALL 		play_note
+		MOV		R4, MIDE	
+		MOV		R31, SIXTEENTH
+		CALL 		play_note
+		MOV		R4, REST	
+		MOV		R31, EIGHTH
+		CALL 		play_note
+		MOV		R4, MIDD	
+		MOV		R31, EIGHTH
+		CALL 		play_note
+		MOV		R4, MIDC	
+		MOV		R31, HALF
+		CALL 		play_note
+		MOV		R4, LOB		 	
+		MOV		R31, HALF
+		CALL 		play_note
+		RET
+		;BRN		Seven_NA
 
 
 
@@ -37,6 +113,9 @@ play_note: 	OUT R4, SPEAKER_PORT
 ; 	- R31: outer for loop count
 ; Affected:
 ; 	- R1, R2, R3
+; TODO:
+; 	- Add another loop so delay will last note length
+; 	- Make subroutine to calculate length of 1/16 note, then make base delay be equal to that time
 
 delay: 		MOV R1, R31  ;set outside for loop count
 outside_one: 	SUB R1, 0x01
