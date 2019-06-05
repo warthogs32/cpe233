d739f07119b9cc42cf33efe13cffb0b207efacf4
anthonymargulis <48341467+anthonymargulis@users.noreply.github.com>
2019-06-05 13:45:00 -0700

    stairway to heaven added

--- Music.asm
+++ Music.asm
@@ -1,133 +1,123 @@
 ; BPM constants
-.EQU 		BPM_126 = 11
-.EQU 		BPM_106 = 13
-.EQU 		BPM_78 = 18
-
-; DELAY constants
-.EQU 	 	INNER_LOOP = 0xFF
+.EQU 		BPM_126 = 11
+.EQU 		BPM_106 = 13
+.EQU 		BPM_78 = 18
+.EQU		BPM_63 = 23
+; DELAY constants
+.EQU 	 	INNER_LOOP = 0xFF
 .EQU 	 	MIDDLE_LOOP = 0xFF
-
-; PORT Definitions
-.EQU 	 	SPEAKER_PORT = 0x69
-.EQU 	 	LOWER_SWITCHES = 0x21
-
+; PORT Definitions
+.EQU 	 	SPEAKER_PORT = 0x69
+.EQU 	 	LOWER_SWITCHES = 0x21
+
 ; NOTE Defintions
-
 .EQU		THIRS	= 0x01
-
-.EQU 		SIXT 	= 0X02
-.EQU 		EIGHTH 	= 0X04
-.EQU 		QUARTER	= 0X08
-.EQU 	 	HALF 	= 0X10
+.EQU 		SIXT 	= 0X02
+.EQU 		EIGHTH 	= 0X04
+.EQU 		QUARTER	= 0X08
+.EQU 	 	HALF 	= 0X10
 .EQU 	 	WHOLE 	= 0X20
+.EQU 		REST 	= 0
+.EQU 		LOC  	= 1
+.EQU 		LOCS 	= 2
+.EQU 		LOD  	= 3
+.EQU 	 	LODS 	= 4
+.EQU 		LOE  	= 5
+.EQU 		LOF  	= 6
+.EQU 		LOFS 	= 7
+.EQU 		LOG  	= 8
+.EQU 		LOGS 	= 9
+.EQU 		LOA  	= 10
+.EQU 	 	LOAS 	= 11
+.EQU 		LOB  	= 12
+.EQU 		MIDC 	= 13
+.EQU 		MIDCS 	= 14
+.EQU 		MIDD  	= 15
+.EQU 	 	MIDDS 	= 16
+.EQU 		MIDE  	= 17
+.EQU 		MIDF  	= 18
+.EQU 		MIDFS 	= 19
+.EQU 		MIDG  	= 20
+.EQU 		MIDGS 	= 21
+.EQU 		MIDA  	= 22
+.EQU 	 	MIDAS 	= 23
+.EQU 		MIDB  	= 24
+.EQU 		HIC 	= 25
+.EQU 		HICS	= 26
+.EQU 		HID 	= 27
+.EQU 	 	HIDS	= 28
+.EQU 		HIE 	= 29
+.EQU 		HIF 	= 30
+.EQU 		HIFS	= 31
+.EQU 		HIG 	= 32
+.EQU 		HIGS	= 33
+.EQU 		HIA 	= 34
+.EQU 	 	HIAS	= 35
+.EQU 		HIB 	= 36
+.EQU		HIXC	= 37
+.EQU		HIXD	= 39
+.EQU		HIXE 	= 41
+.EQU		HIXF	= 42
+.EQU		HIXG	= 44
+.EQU		HIXA	= 46
+.EQU		HIXB	= 48	
+.EQU		HIXXC	= 49
+; R1, R2, R3 used for delay loop
+; R4 used for note selection
+; R5 used for song selection
+; R30 used for BPM control
+; R31 used for note length selectio
 
-.EQU 		REST 	= 0
-.EQU 		LOC  	= 1
-.EQU 		LOCS 	= 2
-.EQU 		LOD  	= 3
-.EQU 	 	LODS 	= 4
-.EQU 		LOE  	= 5
-.EQU 		LOF  	= 6
-.EQU 		LOFS 	= 7
-.EQU 		LOG  	= 8
-.EQU 		LOGS 	= 9
-.EQU 		LOA  	= 10
-.EQU 	 	LOAS 	= 11
-.EQU 		LOB  	= 12
-.EQU 		MIDC 	= 13
-.EQU 		MIDCS 	= 14
-.EQU 		MIDD  	= 15
-.EQU 	 	MIDDS 	= 16
-.EQU 		MIDE  	= 17
-.EQU 		MIDF  	= 18
-.EQU 		MIDFS 	= 19
-.EQU 		MIDG  	= 20
-.EQU 		MIDGS 	= 21
-.EQU 		MIDA  	= 22
-.EQU 	 	MIDAS 	= 23
-.EQU 		MIDB  	= 24
-.EQU 		HIC 	= 25
-.EQU 		HICS	= 26
-.EQU 		HID 	= 27
-.EQU 	 	HIDS	= 28
-.EQU 		HIE 	= 29
-.EQU 		HIF 	= 30
-.EQU 		HIFS	= 31
-.EQU 		HIG 	= 32
-.EQU 		HIGS	= 33
-.EQU 		HIA 	= 34
-.EQU 	 	HIAS	= 35
-.EQU 		HIB 	= 36
-
-
-; R1, R2, R3 used for delay loop
-; R4 used for note selection
-; R5 used for song selection
-; R30 used for BPM control
-; R31 used for note length selection
-
-
-main: 		IN 		R5, LOWER_SWITCHES 	; Read song number from switches
-			CMP		R5, 1
-			BREQ	Seven_NA
-			CMP		R5, 2
-			BREQ	Real_Slim
-			CMP		R5, 3
-			BREQ	Power
-			CMP		R5, 4
+main: 		IN 		R5, LOWER_SWITCHES 	; Read song number from switches
+			OUT		R5, 0x81
+			CMP		R5, 1
+			BREQ	Seven_NA
+			CMP		R5, 2
+			BREQ	Real_Slim
+			CMP		R5, 3
+			BREQ	Power
+			CMP		R5, 4
 			BREQ Imp_March
-			
+			CMP		R5, 5
+			BREQ	Stairway
 			MOV		R4, REST
-			
-			OUT		R4, SPEAKER_PORT
-		;CALL 		Seven_NA 		; Will be replaced with conditional branches to select correct song 
-			BRN 		main 			; Repeat indefinitely
-
-		
+			OUT		R4, SPEAKER_PORT
+			BRN 		main 			; Repeat indefinitely
 ;The White Stripes  Seven Nation Army  126 BPM
-
-Seven_NA:	MOV 		R30, BPM_126 		; Sets base loop to .12s => 126bpm
-		MOV		R4, MIDE		; Choose note
-		MOV		R31, QUARTER 		; Choose duration
-		CALL 		play_note 		; Play note
-		MOV		R31, EIGHTH
+Seven_NA:	MOV 		R30, BPM_126 		; Sets base loop to .12s => 126bpm
+		MOV		R4, MIDE		; Choose note
+		MOV		R31, QUARTER 		; Choose duration
+		CALL 		play_note 		; Play note
+		MOV		R31, EIGHTH
 		CALL 		play_note
-		
-		MOV		R4, REST	
-		MOV		R31, SIXT
+		MOV		R4, REST	
+		MOV		R31, SIXT
 		CALL 		play_note
-		
-		MOV		R4, MIDE	
-		MOV		R31, EIGHTH
+		MOV		R4, MIDE	
+		MOV		R31, EIGHTH
 		CALL 		play_note
-
-		MOV		R4, MIDG	
-		MOV		R31, EIGHTH
+		MOV		R4, MIDG	
+		MOV		R31, EIGHTH
+		CALL 		play_note
+		
+		MOV		R4, MIDG	
+		MOV		R31, SIXT
 		CALL 		play_note
-		MOV		R4, MIDG	
-		MOV		R31, SIXT
+		MOV		R4, MIDE	
+		MOV		R31, SIXT
 		CALL 		play_note
-
-		MOV		R4, MIDE	
-		MOV		R31, SIXT
+		MOV		R4, REST	
+		MOV		R31, EIGHTH
 		CALL 		play_note
-
-		MOV		R4, REST	
-		MOV		R31, EIGHTH
+		MOV		R4, MIDD	
+		MOV		R31, EIGHTH
 		CALL 		play_note
-
-		MOV		R4, MIDD	
-		MOV		R31, EIGHTH
+		MOV		R4, MIDC	
+		MOV		R31, HALF
 		CALL 		play_note
-
-		MOV		R4, MIDC	
-		MOV		R31, HALF
-		CALL 		play_note
-
-		MOV		R4, LOB		 	
-		MOV		R31, HALF
+		MOV		R4, LOB		 	
+		MOV		R31, HALF
 		CALL 		play_note	
-		
 ;;;;;;;;;;;;;;;; 2nd Stanza ;;;;;;;;;;;;;;;;;;;;;
 		MOV		R4, MIDE		; Choose note
 		MOV		R31, QUARTER 		; Choose duration
@@ -733,6 +723,719 @@ inside_one:   	SUB 		R3, 0x01
 		BRNE 		delay 		; repeat the base delay loop R31 times
 		
       	RET
+
+
+;LED ZEPPELIN!!! -Stairway to Heaven Solo - 63 BPM
+
+
+Stairway:	MOV		R30, BPM_63
+			MOV		R10, 0
+			MOV		R12, 0 
+
+			
+			MOV		R4, HID		; Choose note
+			MOV		R31, HALF 		; Choose duration
+			CALL 		play_note 		; Play note		
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;;;;;;;;2nd stanza;;;;;;;;;;;;;
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDF	
+			MOV		R31, HALF 
+			CALL 		play_note
+
+			MOV		R4, HIG		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+
+			MOV		R4, HIXC		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, SIXT 
+			CALL 		play_note
+
+			MOV		R4, HIXC		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;3rd stanza;;;;;;;;;;;;;;
+
+			MOV		R4, HIXC		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, SIXT 
+			CALL 		play_note
+
+			MOV		R4, HIXC		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+
+			MOV		R4, HIA		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+
+			MOV		R4, HIA		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;;;;;;;;4th Stanza			
+			MOV		R4,  HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIF	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;;;;;;5th Stanza;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+;;;;;;;;;;;;;;6th Stanza;;;;;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, MIDG		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDF		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, MIDF		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;;7th Stanza;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, MIDA		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIF		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG	
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, REST	
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+;;;;;;;;;;;;;;;8th Stanza;;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIXD		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, EIGHTH    
+			CALL 		play_note
+			MOV		R4, HIXG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIXF		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;9th Stanza;;;;;;;;;;;;;;;;;			
+Zep_loop1:	MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			ADD		R10, 1
+			MOV		R11, 10
+			CMP		R10, R11
+			BRNE	Zep_loop1
+;;;;;;;;;;;;;;11th & 12th Stanza;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIXC	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, HALF
+			CALL 		play_note
+			
+			MOV		R4, HIXE	
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;Stanza 13;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIXD
+			MOV		R31, SIXT 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, HALF 		
+			CALL 		play_note
+			MOV		R4, HIA		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+			
+			MOV		R4, REST		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+;;;;;;;;;;Stanza 14;;;;;;;;;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIXD		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXA		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIXG		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIXC
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXD		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;Stanza 15, 16, 17;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, HIXD	
+			MOV		R31, WHOLE
+			CALL 		play_note
+			
+			MOV		R4, REST		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+			MOV		R4, HIA		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			MOV		R4, REST		
+			MOV		R31, THIRS 
+			CALL 		play_note
+			
+			MOV		R4, HIA	
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, REST		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIG		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIA		
+			MOV		R31, WHOLE 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;Stanza 18;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+			
+			MOV		R4, REST	
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+			MOV		R4, HIE		
+			MOV		R31, EIGHTH 
+			CALL 		play_note
+			
+			MOV		R4, REST		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIDS		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+;;;;;;;;;;;;;;;STANZA 19;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HID		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, MIDA		
+			MOV		R31, HALF 
+			CALL 		play_note
+			MOV		R4, MIDA		
+			MOV		R31, QUARTER 
+			CALL 		play_note
+;;;;;;;;;;;;;;;;;;;;;;STANZA 20;;;;;;;;;;;;;;;;;;;;;;;;		
+			MOV		R4, REST		
+			MOV		R31, EIGHTH
+			CALL 		play_note
+			
+			MOV		R4, HIXE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+Zep_Loop2:	MOV		R4, HIXXC		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXA		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			MOV		R4, HIXE		
+			MOV		R31, SIXT 
+			CALL 		play_note
+			
+			ADD		R12, 1
+			MOV		R13, 5
+			CMP		R12, R13
+			BRNE	Zep_loop2
+			
+			MOV		R4, HIXXC		
+			MOV		R31, HALF 
+			CALL 		play_note
+			
+			BRN 	main
+			
+			
+			
+			
+
+
+			
+
+
+
 
 ;ISR:
 ;IN R17, BUTTONS
